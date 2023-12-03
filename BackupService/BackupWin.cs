ae070af4b5b89f3513fb5c1980a3fba4e01857e7
lazicm13 <74549931+lazicm13@users.noreply.github.com>
2023-12-03 13:54:24 +0100

    Replikator added

---
+++ BackupService/BackupWin.cs
@@ -0,0 +1,48 @@
+﻿using Common;
+using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupService
+{
+    public class BackupWin : ChannelFactory<IReplikator>, IReplikator, IDisposable
+    {
+
+        IReplikator factory;
+
+        public BackupWin(NetTcpBinding binding, string address) : base(binding, address)
+        {
+            try
+            {
+                factory = this.CreateChannel();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Greska u konstruktoru " + ex);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (factory != null)
+            {
+                factory = null;
+            }
+
+            this.Close();
+        }
+        public void Posalji(List<Account> studenti)
+        {
+            factory.Posalji(studenti);
+        }
+
+        public List<Account> Preuzmi(DateTime vremeReplikacije)
+        {
+            return factory.Preuzmi(vremeReplikacije);
+        }
+    }
+}
