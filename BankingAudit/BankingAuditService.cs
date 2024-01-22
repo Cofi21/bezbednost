e800d6e77880f35bb510c9909b992d499cf57f48
Cofi21 <bogdanovicfilip212@gmail.com>
2024-01-22 23:56:57 +0100

    final app

--- BankingAudit/BankingAuditService.cs
+++ BankingAudit/BankingAuditService.cs
@@ -18,30 +18,6 @@ namespace BankingAudit
         public void AccessingLog(TransactionPayments tp)
         {
             Console.WriteLine("Izvrsenje...");
-            //string fileInput = string.Empty;
-            //fileInput += "\n---------------------------------------------------------------\n";
-            //fileInput += "Bank name: " + tp.BankName + "\n";
-            //fileInput += "Account name: " + tp.AccountName + "\n";
-            //fileInput += "Time of Detection: " + tp.TimeOfDetection.ToString("dd.MM.yyyy HH:mm:ss") + "\n";
-            //fileInput += "Transactions:";
-            //string numberOfTransactions;
-
-            //foreach (TransactionDetails td in tp.TransactionsList)
-            //{
-            //    numberOfTransactions = Regex.Match(td.ToString(), @"\d+").Value;
-
-            //    fileInput += "\n\n\tTransaction: " + td.TransactionOrderNumber;
-            //    fileInput += "\n\tReceived Time: " + td.ReceivedDateTime.ToString("dd.MM.yyyy HH:mm:ss");
-            //    fileInput += "\n\tAmmount: " + td.Svota;
-            //}
-
-            //fileInput += "\n---------------------------------------------------------------\n";
-
-            //using (StreamWriter sw = new StreamWriter("..\\..\\LogFile.txt", true))
-            //{
-            //    sw.WriteLine(fileInput);
-            //    Console.WriteLine(fileInput);
-            //}
             int index = tp.TransactionsList.Count;
             if (index > 0)
             {
